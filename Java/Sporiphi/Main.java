2a7005895122742301c582bd4674baeb410c9464
Marcos Momenté <vinimomente22@gmail.com>
2025-04-30 19:21:35 -0300

    Atualização Main
    commit para atualização do método main, com a implementação de switch para a classe Gerenciador de Artistas.

--- Java/Sporiphi/Main.java
+++ Java/Sporiphi/Main.java
@@ -4,31 +4,70 @@ public class Main{
     public static void main(String[]args) {
         
         Scanner ler = new Scanner(System.in);
-        System.out.println("--Spotify--");
+        GerenciadorDeArtistas gerenciador = new GerenciadorDeArtistas(ler);
 
-        Artista artista1 = new Artista("Marcos", 0);
-        System.out.println("Artista: " + artista1.getNome() + "Seguidores: " + artista1.getSeguidor());
-
-        int i = 1;
-
-        while (i == 1) {
-            System.out.println("Digite o nome da musica: ");
-            String nomeMusica = ler.nextLine();
-            artista1.upload(nomeMusica);
-            System.out.println("Deseja realizar mais 1 upload ?");
+        int opcao;
+        
+        do {
+            System.out.println("\n-- Sporiphi--");
+            System.out.println("1 - Criar Artista");
+            System.out.println("2 - Selecionar Artista");
+            System.out.println("3. Exibir Artistas");
+            System.out.println("4. Exibir Musicas");
+            System.out.println("0 - Sair");
+            System.out.println("Escolha uma opção: ");
 
             if (ler.hasNextInt()) {
-                i = ler.nextInt();
+                opcao = ler.nextInt();
                 ler.nextLine();
+
+                switch (opcao) {
+                    case 1:
+                        gerenciador.criarArtista();
+                        break;
+                    case 2:
+                        Artista artistaSelecionado = gerenciador.selecionarArtista();
+                        if (artistaSelecionado != null) {
+                            int continuarUpload = 1;
+                            while (continuarUpload == 1) {
+                                System.out.println("Digite o Nome da Musica:");
+                                String nomeMusica = ler.nextLine();
+                                artistaSelecionado.upload(nomeMusica);
+                                System.out.println("Deseja fazer mais um upload para " + artistaSelecionado.getNome() + "? (1 para sim / 0 para não): ");
+                                if (ler.hasNextInt()) {
+                                    continuarUpload = ler.nextInt();
+                                    ler.nextLine();
+                                } else {
+                                    System.out.println("Entrada invalida");
+                                    ler.nextLine();
+                                    continuarUpload = 0;
+                                }
+                                System.out.println("\n--- Músicas de " + artistaSelecionado.getNome() + " ---");
+                                artistaSelecionado.exibirMusica();
+                            }
+                        }
+                        break;
+                    case 3:
+                        gerenciador.exibirArtistas();
+                        break;
+                    case 4:
+                        Artista exibirMusica = gerenciador.selecionarArtista();
+                        if (exibirMusica != null) {
+                            System.out.println("\n-- Musicas de " + exibirMusica.getNome() + "--");
+                            exibirMusica.exibirMusica();
+                        }
+                    case 0:
+                        System.out.println("Saindo do Sporiphi.");
+                    default:
+                        System.out.println("Opção invalida. Tente novamente.");
+                }
             } else {
-                System.out.println("Entrada inválida. Continuando como se fosse 'não'.");
-                i = 0; // Assume 'não' em caso de entrada inválida
+                System.out.println("Entrada invalida. Digite um número.");
                 ler.nextLine();
+                opcao =-1;
             }
-        }
+        } while (opcao !=0);
 
-        System.out.println("\n--- Lista de Músicas ---");
-        artista1.exibirMusica();
-
+        ler.close();
     }
 }
